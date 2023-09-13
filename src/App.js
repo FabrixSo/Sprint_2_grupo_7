136057c724bcda9924f91d38b7c3ac53ff34f177
pablogirardi <pablogirardiv@gmail.com>
2023-09-12 21:56:56 -0300

    Martes12.9

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import calculadoraDePrestamos from "./Components/calculadoraDePrestamos";
+import calculadoraDeMoneda from "./Components/conversorDeModena";
+import cuentas from "./Components/cuentas";
+import informacioDeContactos from "./Components/informacionDeContactos";
+import pagos from "./Components/pagos";
+import politicasDePrivacidad from "./Components/politicasDePrivacidad";
+import terminosYCondiciones from "./Components/terminosYCondiciones";
+import transferencias from "./Components/transferencias";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
